03d32b47cb0517ed329e186402b40e67f304b07b
hyundoria <110231201+hyundoria@users.noreply.github.com>
2025-07-22 20:45:20 +0900

    Title: 격자 위의 편안한 상태, Time: 77ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 250722/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -0,0 +1,28 @@
+n, m = map(int, input().split())
+
+arr = [[0] * n for _ in range(n)]
+
+move = [(-1,0),(0,1),(1,0),(0,-1)]
+
+for _ in range(m):
+
+    r, c = map(int, input().split())
+    r ,c = r-1, c-1
+    arr[r][c] = 1
+
+    cnt = 0
+
+    for dx, dy in move:
+        nx = r + dx
+        ny = c + dy
+
+        if nx < 0 or nx >= n or ny < 0 or nx >= n:
+            continue
+
+        if arr[nx][ny] == 1:
+            cnt += 1
+
+    if cnt == 3:
+        print(1)
+    else:
+        print(0)
