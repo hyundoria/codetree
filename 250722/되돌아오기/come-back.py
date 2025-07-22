96e06024633758809a26599d313f562849650dcd
hyundoria <110231201+hyundoria@users.noreply.github.com>
2025-07-22 20:37:54 +0900

    Title: 되돌아오기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250722/되돌아오기/come-back.py
@@ -0,0 +1,28 @@
+n = int(input())
+
+direction = {
+    'N':(0,1),
+    'E':(1,0),
+    'S':(0,-1),
+    'W':(-1,0)
+}
+
+x, y = 0, 0
+
+sec = 0
+
+for _ in range(n):
+
+    d, l = input().split()
+    l = int(l)
+
+    for _ in range(l):
+
+        dx = x + direction[d][0]
+        dy = y + direction[d][1]
+        sec += 1
+
+        if dx == 0 and dy == 0:
+            print(sec)
+            exit(0)
+        x, y = dx, dy
