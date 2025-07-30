be81be0d49eab856f22cae280e749269e413c5a5
hyundoria <110231201+hyundoria@users.noreply.github.com>
2025-07-30 16:36:10 +0900

    Title: 아름다운 수, Time: 201ms, Memory: 23MB, Status: Wrong Answer - Codetree

---
+++ 250730/아름다운 수/beautiful-number.py
@@ -0,0 +1,45 @@
+n = int(input())
+
+count = []
+
+ans = []
+
+def get(num):
+
+    if num == n+1:
+
+        cnt = 0
+
+        if ans[0] != 1:
+            cnt = 1
+
+        for i in range(1, n):
+            
+            if ans[i-1] == ans[i]:
+                cnt += 1
+                
+                cnt = cnt % ans[i]
+            else:
+                if cnt != 0:
+                    return
+                else:
+                    if ans[i] != 1:
+                        cnt += 1
+
+        if cnt != 0:
+            return
+        
+        count.append(1)
+        return
+
+
+    for i in range(1, n+1):
+        ans.append(i)
+        get(num+1)
+        ans.pop()
+
+
+    return
+
+get(1)
+print(len(count))
