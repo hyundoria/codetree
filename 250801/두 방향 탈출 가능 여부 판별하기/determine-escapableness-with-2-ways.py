21e06eef18a474b010e78420597ce313289318b9
hyundoria <110231201+hyundoria@users.noreply.github.com>
2025-08-01 14:49:08 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 73ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250801/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+n, m = map(int, input().split())
+
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+visited = [[False] * n for _ in range(m)]
+
+dxy = [[1,0],[0,1]]
+
+def bfs(x,y):
+
+    q = deque()
+    q.append((x,y))
+    visited[x][y] = True
+
+    while q:
+        x,y = q.popleft()
+
+        for dx,dy in dxy:
+
+            nx = x + dx
+            ny = y + dy
+
+            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and grid[nx][ny] == 1:
+                visited[nx][ny] = True
+                q.append((nx,ny))
+
+bfs(0,0)
+
+if not visited[n-1][m-1]:
+    print(0)
+else:
+    print(1)
+
+
+
