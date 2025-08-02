61251ca8694a236597fb86250ca2835bd245e431
hyundoria <110231201+hyundoria@users.noreply.github.com>
2025-08-02 20:01:52 +0900

    Title: 최대 증가 부분 수열, Time: 57ms, Memory: 17MB, Status: Passed - Codetree

--- 250802/최대 증가 부분 수열/longest-increasing-subsequence.py
+++ 250802/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -1,20 +1,12 @@
-import sys
-
-INT_MIN = -sys.maxsize
-
 n = int(input())
 
 sequence = list(map(int, input().split()))
 
-dp = [INT_MIN] * n
-dp[0] = 1
+dp = [1] * n
 
 for i in range(n):
 
     for j in range(i):
-        
-        if dp[j] == INT_MIN:
-            continue
         
         if sequence[j] < sequence[i]:
             dp[i] = max(dp[i], dp[j] + 1)
